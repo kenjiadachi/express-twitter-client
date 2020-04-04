9ae7afa4eedb73cef95b05333ac3968aea5816ea
延知奈美 <chinami@enchinaBookpuro.lan>
2020-04-05 00:21:52 +0900

    keyword.jsの修正

--- func/keywords.js
+++ func/keywords.js
@@ -1,16 +1,28 @@
 
-var fs = require('fs');
-const filepath = '/settings.json'
+const fs = require('fs');
+const path = require('path');
+const filepath = '/data/settings.json'
 
-result = save_keywords(['k1','kn'], '3');
-console.log(result);
+result = save_keywords(['new','dde'], 'idn4');
+
 //キーワードを保存する
 function save_keywords(keywords,user_id) {
-  let jsonObject = {};
-  if(fs.existsSync(__dirname + filepath)) {
+  let jsonObject = [];
+  if(fs.existsSync(path.join( __dirname, '../', filepath))) {
     console.log("settings.json file exists.");
-    jsonObject = JSON.parse(fs.readFileSync(__dirname + filepath, 'utf8'));
-  //  if あれば
+    jsonObject = JSON.parse(fs.readFileSync(path.join( __dirname, '../', filepath), 'utf8'));
+
+    let search = jsonObject.findIndex((v) => v.id === user_id);
+
+    if(search!=-1){
+      console.log(jsonObject[search].keywords);
+      jsonObject[search].keywords = keywords;
+    }else{
+      jsonObject.push({
+        id: user_id,
+        keywords: keywords
+      });
+    }
   //  jsonObjectが配列やから、その配列内のオブジェクトから、オブジェクト内のuser＿idが同じものがあるかどうかを探索
   //  あれば、そのオブジェクト内の、keywordsを置き換える
   //  なければ
@@ -22,14 +34,14 @@ function save_keywords(keywords,user_id) {
   }else {
     console.log('keyword file does not exist');
     //keyword.jsonがないときの処理
-    jsonObject = {
+    jsonObject.push({
       id: user_id,
       keywords: keywords
-    };
+    });
     console.log(jsonObject);
   }
   //new Objectをファイルに書き出し
-  fs.writeFile( __dirname + filepath, JSON.stringify(jsonObject) , (err) => {
+  fs.writeFile( path.join( __dirname, '../', filepath), JSON.stringify(jsonObject) , (err) => {
     // 書き出しに失敗した場合
     if(err){
       console.log("エラーが発生しました。" + err)
@@ -41,5 +53,4 @@ function save_keywords(keywords,user_id) {
     }
   });
 
-  return jsonObject[user_id];
 };
