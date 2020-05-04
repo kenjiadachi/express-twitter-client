5be4924b896916e6dcff3287fd2300f8cfbaa491
安達 健二 <kenji.183927@gmail.com>
2020-05-04 15:28:59 +0900

    rssほぼ完成

--- batch/rss.js
+++ batch/rss.js
@@ -3,60 +3,100 @@ var fs = require('fs');
 const path = require('path');
 const diff = require('../func/diff');
 const axios = require('axios');
+const twitter = require('../func/twitter');
+const format = require('../func/format');
+const filename = path.join( __dirname, '../data/', 'settings.json');
 
 const now = new Date();
 const date = now.getFullYear() + "-" + ("0" + (now.getMonth()+1)).slice(-2) + "-" + ("0" + now.getDate()).slice(-2) + " " + ("0" + now.getHours()).slice(-2) + ":" + ("0" + now.getMinutes()).slice(-2) + ":" + ("0" + now.getSeconds()).slice(-2);
 
-// rss();
+rss();
 
 async function rss(){
   let filepath = path.join( __dirname, '../data/rss');
   let fileList = [];
   fileList = fs.readdirSync(filepath);
-
+  const jsonObject = JSON.parse(fs.readFileSync(filename, 'utf8'));
   for (var jsonFile of fileList){
-    let result = [];
-    let rssObject = JSON.parse(fs.readFileSync(path.join( __dirname, '../data/rss', jsonFile), 'utf8'));
-    let logsFileName = path.join( __dirname, '../logs/rss-logs/', jsonFile);
-    for(var xmlfile of rssObject) {
-      let json = {};
-      let existing_items = [];
+    console.log(fileList);
+    // もし、settings.jsonのなかに、jsonFileの名前がIDとして存在すれば
+    if (jsonObject.findIndex((v) => v.id === jsonFile.split('.')[0]) != -1) {
+      const settingData = jsonObject.find((v) => v.id === jsonFile.split('.')[0]);
+      console.log(settingData);
+      // そのオブジェクトの中に、token, tokenSecertが存在すれば
+      if (Object.keys(settingData).indexOf('token') !== -1 && Object.keys(settingData).indexOf('tokenSecret') !== -1) {
 
-      // rss-logs内に自分のファイルが存在すれば
-      if(fs.existsSync(logsFileName)){
-        var logsObject = JSON.parse(fs.readFileSync(logsFileName), 'utf8');
+        const client = twitter.initWithToken(settingData.token, settingData.tokenSecret);
 
-        result = logsObject;
+        let result = [];
+        let newObj = [];
+        
+        let rssObject = JSON.parse(fs.readFileSync(path.join( __dirname, '../data/rss', jsonFile), 'utf8'));
+        let logsFileName = path.join( __dirname, '../logs/rss-logs/', jsonFile);
+        for(var xmlfile of rssObject) {
+          let json = {};
+          let existing_items = [];
 
-        for(var item of logsObject){
-          if(item.url === xmlfile.url){
-            existing_items = item.content.rss.channel.item;
-            // console.log(existing_items);
+          // rss-logs内に自分のファイルが存在すれば
+          if(fs.existsSync(logsFileName)){
+            var logsObject = JSON.parse(fs.readFileSync(logsFileName), 'utf8');
+            result = logsObject;
+            
+
+            for(let item of logsObject){
+              if(item.url === xmlfile.url){
+                existing_items = existing_items.concat(item.content.rss.channel.item);
+              }
+            }
+          }
+
+          // console.log(xmlfile.url);
+          await axios.get(xmlfile.url)
+          .then(response => {
+            // console.log(response.data);
+            let options = {};
+            options.object = true;
+            json = parser.toJson(response.data, options);
+            var thisItems = json.rss.channel.item;
+            var newItems = diff.ObjectArrays(thisItems, existing_items, "link").onlyObjArr1;
+
+            let resultObj = {
+              url: xmlfile.url,
+              created_at: date,
+              content: json,
+              newItems: newItems
+            };
+            newObj.push(resultObj);
+          }).catch(err => {
+            console.log('err:', err);
+          });
+        }
+
+        // newItemそれぞれに対してAPIを叩く
+        for(let obj of newObj){
+          for(let item of obj.newItems){
+          
+            // もし、投稿すべき内容であれば
+            if (true) {
+              let options = {};
+              options.status = item.title + "\n" +item.link;
+              // APIを叩く
+              try {
+                await client.post('statuses/update', options)
+                .then((res) => {
+                  console.log(res);
+                  item.tweet = format.tweet(res);
+                });
+              } catch (err) {
+                console.log(err);
+              }
+            }
           }
         }
-      }
 
-      // console.log(xmlfile.url);
-      await axios.get(xmlfile.url)
-      .then(response => {
-        // console.log(response.data);
-        let options = {};
-        options.object = true;
-        json = parser.toJson(response.data, options);
-        var thisItems = json.rss.channel.item;
-        var newItems = diff.ObjectArrays(thisItems, existing_items, "link").onlyObjArr1;
-        let resultObj = {
-          url: xmlfile.url,
-          created_at: date,
-          content: json,
-          newItems: newItems
-        };
-        result.push(resultObj);
-      }).catch(err => {
-        console.log('err:', err);
-      });
+        saveToJson(logsFileName, result.concat(newObj));
+      }
     }
-    saveToJson(logsFileName, result);
   }
 
 }
